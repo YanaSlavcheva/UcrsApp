30622a4628283b56c0561f29e78185052266fb91
Yana Slavcheva <yana.slavcheva@gmail.com>
2017-12-07 01:45:02 +0200

    Added validation for Title and Points in CourseBindingModel (matching db constraints)

--- Web/Ucrs.Web/ViewModels/Courses/CourseBindingModel.cs
+++ Web/Ucrs.Web/ViewModels/Courses/CourseBindingModel.cs
@@ -1,13 +1,18 @@
 ﻿namespace Ucrs.Web.ViewModels.Courses
 {
     using System.ComponentModel.DataAnnotations;
-
+    using Ucrs.Common;
     using Ucrs.Data.Models;
     using Ucrs.Web.Infrastructure.Mapping;
 
     public class CourseBindingModel : IMapTo<Course>
     {
         [Required]
+        [MaxLength(GlobalConstants.CourseTitleMaxLength)]
         public string Title { get; set; }
+
+        [Required]
+        [Range(GlobalConstants.CoursePointsMinValue, GlobalConstants.CoursePointsMaxValue)]
+        public int Points { get; set; }
     }
 }
