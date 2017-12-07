4320e02114784a25f62da5e5049ac7d4ac929a52
Yana Slavcheva <yana.slavcheva@gmail.com>
2017-12-08 00:11:10 +0200

    Added InMemory repositories

---
+++ Data/Ucrs.Data/Repositories/InMemoryDeletableEntityRepository.cs
@@ -0,0 +1,46 @@
+﻿namespace Ucrs.Data.Repositories
+{
+    using System;
+    using System.Linq;
+    using Ucrs.Data.Common.Models;
+    using Ucrs.Data.Common.Repositories;
+
+    public class InMemoryDeletableEntityRepository<T, TKey> : InMemoryRepository<T, TKey>, IDeletableEntityRepository<T>
+        where T : class, IIdentifyable<TKey>, IDeletableEntity
+    {
+        public override IQueryable<T> All()
+        {
+            return base.All().Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> AllWithDeleted()
+        {
+            return base.All();
+        }
+
+        public override void Delete(T entity)
+        {
+            var databaseEntity = this.DatabaseStore.FirstOrDefault(x => x.Id.Equals(entity.Id));
+            if (databaseEntity != null)
+            {
+                databaseEntity.IsDeleted = true;
+                databaseEntity.DeletedOn = DateTime.Now;
+            }
+        }
+
+        public void HardDelete(T entity)
+        {
+            base.Delete(entity);
+        }
+
+        public void Undelete(T entity)
+        {
+            var databaseEntity = this.DatabaseStore.FirstOrDefault(x => x.Id.Equals(entity.Id));
+            if (databaseEntity != null)
+            {
+                databaseEntity.IsDeleted = false;
+                databaseEntity.DeletedOn = null;
+            }
+        }
+    }
+}
---
+++ Data/Ucrs.Data/Repositories/InMemoryRepository.cs
@@ -0,0 +1,61 @@
+﻿namespace Ucrs.Data.Repositories
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Ucrs.Data.Common.Models;
+    using Ucrs.Data.Common.Repositories;
+
+    public class InMemoryRepository<T, TKey> : IRepository<T>
+        where T : class, IIdentifyable<TKey>
+    {
+        protected readonly IList<T> DatabaseStore;
+
+        public InMemoryRepository()
+        {
+            this.DatabaseStore = new List<T>();
+        }
+
+        public virtual IQueryable<T> All()
+        {
+            return this.DatabaseStore.AsQueryable();
+        }
+
+        public void Add(T entity)
+        {
+            this.DatabaseStore.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            this.Delete(entity);
+            this.Add(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            this.DatabaseStore.Remove(entity);
+        }
+
+        public T GetById(params object[] id)
+        {
+            return this.DatabaseStore.FirstOrDefault(x => id.Any(y => x.Id.Equals(y)));
+        }
+
+        public Task<T> GetByIdAsync(params object[] id)
+        {
+            var entity = this.DatabaseStore.FirstOrDefault(x => id.Any(y => x.Id.Equals(y)));
+            return Task.FromResult(entity);
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return Task.FromResult(0);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
